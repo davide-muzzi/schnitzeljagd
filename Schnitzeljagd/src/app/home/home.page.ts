b8cc32116c9819cb2884d3e8c25f8fce597d6a6b
Erion Jasiqi <erion_jasiqi@sluz.ch>
2026-01-21 15:04:13 +0100

    feat: add functional start button and nonfunctional alert

--- Schnitzeljagd/src/app/home/home.page.ts
+++ Schnitzeljagd/src/app/home/home.page.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonAlert } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonAlert],
 })
 export class HomePage {
-  constructor() {}
+
+  public startButton = [
+    {
+      text: 'Cancel',
+      role: 'cancel',
+    },
+    {
+      text: 'Add',
+      role: 'confirm'
+    },
+  ];
+  constructor() { }
+
+
+  public startInput = [
+    {
+      name: 'Name',
+      type: 'string',
+      placeholder: 'Name',
+      attribute: 'required',
+    }
+
+  ];
 }
