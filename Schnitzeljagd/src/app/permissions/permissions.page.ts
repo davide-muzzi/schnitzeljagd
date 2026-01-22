74a3e6340bb7fab6ffc90c00df3c9338e0bfa195
Erion Jasiqi <erion_jasiqi@sluz.ch>
2026-01-22 09:39:32 +0100

    fix: add abort functionality to back button

--- Schnitzeljagd/src/app/permissions/permissions.page.ts
+++ Schnitzeljagd/src/app/permissions/permissions.page.ts
@@ -2,17 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Router, RouterLink } from '@angular/router';
+import { GameService } from '../services/game';
 
 @Component({
   selector: 'app-permissions',
   templateUrl: './permissions.page.html',
   styleUrls: ['./permissions.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, RouterLink, CommonModule, FormsModule]
 })
 export class PermissionsPage implements OnInit {
 
-  constructor() { }
+  constructor(private gameService: GameService, private router: Router) { }
+
+  abort() {
+    console.log('Game aborted');
+    this.gameService.abort();
+    this.router.navigate(['/home']);
+  }
+
+
 
   ngOnInit() {
   }
